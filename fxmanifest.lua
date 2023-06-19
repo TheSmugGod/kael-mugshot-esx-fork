a0a20aa356b4f967bedadfe4d884f70c40b8e04f
Smug God <123635357+TheSmugGod@users.noreply.github.com>
2023-06-19 12:24:04 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,14 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
  
-description 'A Mugshot Script For Police Job Made By Kael Team'
-version '1.0.3'
+description 'A Mugshot Script For Police Job Made By Kael Team and modified by SmugGOD to work for ESX'
+version '1.0.3ESX'
 author 'Kael-Scripts'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
 client_scripts {
 	'client/**.lua'
