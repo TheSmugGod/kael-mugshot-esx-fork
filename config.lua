631e969e0b7238504bf722129779de3a15531b2a
Smug God <123635357+TheSmugGod@users.noreply.github.com>
2023-06-19 02:22:20 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,19 +1,19 @@
 Config = {
-    Debug = true,
-    Core ="qb-core",
-    Target = "qb-target"
+    Debug = false, -- enable this to see target zone
+    Core ="es_extended",
+    Target = "ox_target"
 }
 
 Config.PoliceJobName = "police"
-Config.BoardHeader = "Your Server Name"
-Config.TargetLoc = vector3(473.01, -1012.57, 26.27)
-Config.MugShotCoords = vector3(472.92, -1011.49, 25.27)
-Config.MugShotHeading = 183.55
+Config.BoardHeader = "Server Name or Something"
+Config.TargetLoc = vector3(1850.2442626953, 3686.2336425781, 36.484786987305)
+Config.MugShotCoords = vector3(1848.18, 3686.584, 33.219)
+Config.MugShotHeading = 294.011
 Config.CameraPos = {
-    pos = vector3(473.05, -1012.56, 26.90),
-    rotation = vector3(0.0,  0.0, 358.04),
+    pos = vector3(1849.116, 3687.272, 34.879),
+    rotation = vector3(0.0,  0.0, 119.249),
 }
 
-Config.LogTitle = "Kael Mugshot"
-Config.LogName = "Kael Mugshot"
-Config.LogIcon = "https://cdn.discordapp.com/attachments/1026588960207667321/1114942675951562844/border-01-tebex.png"
+Config.LogTitle = "ESX Mugshot"
+Config.LogName = "ESX Mugshot"
+Config.LogIcon = "https://images-ext-1.discordapp.net/external/_PZM5usJlD_6hyGqe0OCrMb5k4vBD6KWgyZWElNOEew/%3Fwidth%3D220%26height%3D217/https/images-ext-1.discordapp.net/external/B8GNbCzbtwqBfNeSt44ryMk49EY1X9ejceAfAXhwQl0/https/www.oklahomacounty.org/Portals/0/ThemePluginPro/uploads/2023/3/13/OklahomaCountySeal-Lighter-6_200.png?width=220&height=217"
